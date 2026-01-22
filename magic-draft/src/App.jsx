743deaa9b90db18d19482020e83d532f3b5489f6
Ilham Ishak <ilhamsahak@gmail.com>
2026-01-22 22:18:24 +0800

    change

--- magic-draft/src/App.jsx
+++ magic-draft/src/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Join from "./pages/Join";
 import Referee from "./pages/Referee";
+import Join from "./pages/Join";
 import Room from "./pages/Room";
-import Summary from "./pages/Summary";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Draft-simulation-Magic-Chess">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/join" element={<Join />} />
         <Route path="/referee" element={<Referee />} />
+        <Route path="/join" element={<Join />} />
         <Route path="/room/:roomId" element={<Room />} />
-        <Route path="/summary/:roomId" element={<Summary />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
