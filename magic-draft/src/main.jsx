3b935452d9ee2863979194775fa752e770b459dd
ilhamsahak <37360980+ilhamsahak@users.noreply.github.com>
2026-01-22 21:37:51 +0800

    Update main.jsx

--- magic-draft/src/main.jsx
+++ magic-draft/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const redirect = sessionStorage.redirect
+if (redirect) {
+  sessionStorage.removeItem('redirect')
+  history.replaceState(null, null, redirect)
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
